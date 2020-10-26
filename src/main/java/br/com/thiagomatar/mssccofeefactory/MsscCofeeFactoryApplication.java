5f7eec4afd7ed1e41faeddebde60750bb9a78d87
Thiago Matar <thiago.queiroz@tqi.com.br>
2020-10-26 10:04:31 -0300

    Initial Commit

---
+++ src/main/java/br/com/thiagomatar/mssccofeefactory/MsscCofeeFactoryApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiagomatar.mssccofeefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscCofeeFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscCofeeFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thiagomatar/mssccofeefactory/MsscCofeeFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagomatar.mssccofeefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscCofeeFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
